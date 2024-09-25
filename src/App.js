7420baecdfb12932d1465d65d46225b68f3d296a
Daniel Cortina <danny2003cc@gmail.com>
2024-09-25 19:47:53 -0400

    fetch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 function App() {
-  
-  fetch("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ad0ff359859a4da0b612fa3afa048cdd")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  })
+  const [quote, setQuote]= useState('')
+  const getQuote = () => {
+    axios.get("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ad0ff359859a4da0b612fa3afa048cdd") // https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ad0ff359859a4da0b612fa3afa048cdd
+    .then(res => {
+      console.log(res.data.fact)
+      setQuote(res.data.fact)
+    }).catch(err =>{
+      console.log(err)
+    })
 
-    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+      <button onClick={getQuote}>Get Weather</button>
+      {quote && <p>{quote}</p>}
     </div>
   );
+ 
 }
 
 export default App;
