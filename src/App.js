c78012116fd07efd9c72c0ef75731ca68a515577
gripsama <140536483+gripsama@users.noreply.github.com>
2024-09-27 15:02:47 -0400

    api fetch without axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+import FetchApi from './FetchApi';
 
 
 function App() {
-  const [quote, setQuote]= useState('')
-  const getQuote = () => {
-    axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-08-26&sortBy=publishedAt&apiKey=ad0ff359859a4da0b612fa3afa048cdd") 
-    .then(res => {
-      console.log(res.data.fact)
-      setQuote(res.data.fact)
-    }).catch(err =>{
-      console.log(err)
-    })
+  
+  fetch("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ad0ff359859a4da0b612fa3afa048cdd")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
 
-  };
-
+    
   return (
-    <div className ="App">
-      <button onClick={getQuote}>Get Weather</button>
-      {quote && <p>{quote}</p>}
+    <div className="App">
+
+<FetchApi/>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
- 
 }
 
 export default App;
