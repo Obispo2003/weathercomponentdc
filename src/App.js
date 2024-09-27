d98fb6bd8e3ed286f5be9938aa73464c5a5b55e3
gripsama <140536483+gripsama@users.noreply.github.com>
2024-09-26 20:05:35 -0400

    api was having issues
    changed to a functional api fetch on the same website

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 function App() {
   const [quote, setQuote]= useState('')
   const getQuote = () => {
-    axios.get("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ad0ff359859a4da0b612fa3afa048cdd") // https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ad0ff359859a4da0b612fa3afa048cdd
+    axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-08-26&sortBy=publishedAt&apiKey=ad0ff359859a4da0b612fa3afa048cdd") 
     .then(res => {
       console.log(res.data.fact)
       setQuote(res.data.fact)
